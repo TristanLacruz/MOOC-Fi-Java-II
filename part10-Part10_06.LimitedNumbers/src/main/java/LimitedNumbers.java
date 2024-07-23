c679d38ad1367b2afe9177cc65c94762dbca76b6
Tristán Lacruz <lacruztristan@gmail.com>
2024-07-23 23:51:59 +0200

    Exercise Part10-5-PrintingUserInput & Part10-6-LimitedNumbers

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,16 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = scanner.nextInt();
+            if(input < 0){
+                break;
+            }
+            list.add(input);
+        }
+        list.stream()
+                .filter(i -> i >= 1 && i <= 5)
+                .forEach(element -> System.out.println(element));
     }
 }
