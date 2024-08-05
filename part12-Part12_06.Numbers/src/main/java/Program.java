e7340f1cd6c21e8a6a3792ecbb1c45a0070b43a9
Tristán Lacruz <lacruztristan@gmail.com>
2024-08-05 11:50:32 +0200

    Exercises part12-04, 05, 06, 07 and 08

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,16 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many random numbers should be printed?");
+        int count = Integer.valueOf(scanner.nextLine());
+        printRandomNumbers(count);
+    }
+    
+    public static void printRandomNumbers(int count){
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            System.out.println(random.nextInt(11));
+        }
     }
 
 }
